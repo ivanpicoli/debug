8553bb70832c91595336130587aa533fea81a0a1
Ivan debug <ivpi@itu.dk>
2015-12-11 03:12:08 +0100

    2

--- create.sh
+++ create.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-/home/ivan/lightnvm-adm/lnvm create -d phantomn0 -n phantomn_from_hell -t rrpc_debug -o 0:0
+/home/itu-debug/lightnvm-adm/lnvm create -d phantomn0 -n phantomn_from_hell -t rrpc_debug -o 0:0
 
